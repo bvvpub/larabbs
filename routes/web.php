2fdcaa63fd93b7e1be8ce2c98212c2afa2e27548
bvvpub <115177220@qq.com>
2021-07-06 17:24:28 +0800

    生成用户认证代码

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+
+
+Auth::routes (['verify' => true]);
 
